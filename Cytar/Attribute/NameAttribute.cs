b968892ef699c85b422d08f37005c11bf18d3839
SardineFish <Sardine@live.cn>
2017-11-27 10:50:59 +0800

    Abandon API name encoding

--- Cytar/Attribute/NameAttribute.cs
+++ Cytar/Attribute/NameAttribute.cs
@@ -11,12 +11,12 @@ namespace Cytar
         
         public NameAttribute(string name)
         {
-            if (name.Length > 5)
+            /*if (name.Length > 5)
                 throw new Exception("Over 5 chars.");
             if (!Regex.IsMatch(name, @"^[a-zA-Z0-9_-]+$"))
             {
                 throw new Exception("Invalid Identifier");
-            }
+            }*/
             Name = name;
         }
 
@@ -26,9 +26,10 @@ namespace Cytar
         {
             get
             {
-                char[] fill = new char[] { 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A' };
+                /*char[] fill = new char[] { 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A' };
                 Name.Replace("-", "+").Replace("_", "/").CopyTo(0, fill, 0, Name.Length);
-                return CytarConvert.BytesToUInt32(Convert.FromBase64CharArray(fill, 0, 8));
+                return CytarConvert.BytesToUInt32(Convert.FromBase64CharArray(fill, 0, 8));*/
+                throw new NotImplementedException();
             }
         }
     }
